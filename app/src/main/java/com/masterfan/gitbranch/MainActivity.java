b8f6072e84215954e70e3481d98345f08fc23da4
masterfan <343887511@qq.com>
2016-07-04 22:12:16 +0800

    添加版本2的注释

--- app/src/main/java/com/masterfan/gitbranch/MainActivity.java
+++ app/src/main/java/com/masterfan/gitbranch/MainActivity.java
@@ -3,6 +3,9 @@ package com.masterfan.gitbranch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 版本2的注释
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
